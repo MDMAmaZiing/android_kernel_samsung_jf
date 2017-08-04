34782bdb1c21eed299fa41e5f4045fef57e3d67b
Buff99 <buffbuffster@gmail.com>
2017-08-04 01:24:38 +0100

    Revert "fix cpufreq.h"
    This reverts commit 1338348e2e8fcfd4300fc6ba699acf85248bd776.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -354,12 +354,12 @@ static inline unsigned int cpufreq_quick_get_max(unsigned int cpu)
   Performance governor is fallback governor if any other gov failed to
   auto load due latency restrictions
 */
-#ifdef CONFIG_CPU_FREQ_GOV_PERFORMANCE
--extern struct cpufreq_governor cpufreq_gov_performance;
+#ifdef CONFIG_CPU_FREQ_GOV_INTERACTIVE
+extern unsigned int cpufreq_interactive_get_hispeed_freq(int cpu);
 #endif
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
+extern struct cpufreq_governor cpufreq_gov_performance;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_performance)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
 extern struct cpufreq_governor cpufreq_gov_powersave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_USERSPACE)
